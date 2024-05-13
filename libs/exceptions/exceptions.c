9916718229fbafe77fab416f1112d473ccdd2e0c
primakkk <164992443+primakkk@users.noreply.github.com>
2024-05-13 23:51:25 +0700

    part 3

--- libs/exceptions/exceptions.c
+++ libs/exceptions/exceptions.c
@@ -1,19 +1,9 @@
 #include "exceptions.h"
 
 
-void exceptBadAlloc(){
-    fprintf(stderr, "bad alloc");
-    exit(1);
-}
-
-
-void exceptEmptyVector(){
-    fprintf(stderr, "Vector is empty");
-    exit(1);
-}
-
-
-void exceptIndexError(){
-    fprintf(stderr, "Index Error");
-    exit(1);
+void exceptBadAlloc(vector v){
+    if (!v.data){
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
 }
--- libs/exceptions/exceptions.h
+++ libs/exceptions/exceptions.h
@@ -2,15 +2,8 @@
 #define INC_EXCEPTIONS_H
 
 #include "data_structures/vector/vector.h"
-#include "data_structures/vector/vectorVoid.h"
 
 // ошибка нехватки памяти
-void exceptBadAlloc();
-
-// ошибка - вектор пустой
-void exceptEmptyVector();
-
-// ошибка ненайденного элемента
-void exceptIndexError();
+void exceptBadAlloc(vector v);
 
 #endif
