067e593851e893580c22062ca90ad281855194d9
primakkk <164992443+primakkk@users.noreply.github.com>
2024-05-13 23:47:43 +0700

    vector ADD

---
+++ libs/data_structures/vector/vector.c
@@ -0,0 +1,24 @@
+#ifndef INC_VECTOR_H
+#define INC_VECTOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct vector {
+    int *data; // указатель на элементы вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+} vector;
+
+// возвращает пустой вектор, в который можно вставить n элементов
+vector createVector(size_t n);
+
+// изменяет количество памяти, выделенное под хранение элементов вектора
+void reserve(vector *v, size_t newCapacity);
+
+
+//освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFit(vector *v);
+
+// освобождает память, выделенную вектору
+void deleteVector(vector *v);
